e2f5a975ffb7a8d5c9273544a9da9d6113852dfb
Wandy <wandy20number@gmail.com>
2017-08-20 22:04:57 +0700

    Intent with Data

--- MyIntentApp/app/src/main/java/wansu/example/com/myintentapp/MainActivity.java
+++ MyIntentApp/app/src/main/java/wansu/example/com/myintentapp/MainActivity.java
@@ -7,22 +7,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private Button btnMoveActivity;
+    private Button btnMoveActivity;
+    private Button btnMoveWithDataActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMoveActivity = (Button)findViewById(R.id.btn_move_activity);
+        btnMoveActivity = (Button) findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
+        btnMoveWithDataActivity = (Button) findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_move_activity:
                 Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
+                break;
+
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Wandy");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 24);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_REGION, "Indonesia");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_TODO, "Learning Intent From Dicoding Academy");
+                startActivity(moveWithDataIntent);
                 break;
         }
     }
