1cbebd3ba4e918b1a06247ecb863d4cc93696eca
Wandy <wandy20number@gmail.com>
2017-08-22 23:42:09 +0700

    Intent dengan ResultActivity

--- MyIntentApp/app/src/main/java/wansu/example/com/myintentapp/MainActivity.java
+++ MyIntentApp/app/src/main/java/wansu/example/com/myintentapp/MainActivity.java
@@ -1,15 +1,21 @@
 package wansu.example.com.myintentapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnMoveActivity;
     private Button btnMoveWithDataActivity;
     private Button btnMoveWithObject;
+    private Button btnDialPhone;
+    private Button btnMoveForResult;
+    private TextView tvResult;
+    private int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnMoveActivity.setOnClickListener(this);
         btnMoveWithDataActivity = (Button) findViewById(R.id.btn_move_activity_data);
         btnMoveWithDataActivity.setOnClickListener(this);
-        btnMoveWithObject = (Button)findViewById(R.id.btn_move_activity_object);
+        btnMoveWithObject = (Button) findViewById(R.id.btn_move_activity_object);
         btnMoveWithObject.setOnClickListener(this);
+        btnDialPhone = (Button) findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+        btnMoveForResult = (Button) findViewById(R.id.btn_move_for_result);
+        btnMoveForResult.setOnClickListener(this);
+        tvResult = (TextView) findViewById(R.id.tv_result);
     }
 
     @Override
@@ -51,6 +62,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, mPerson);
                 startActivity(moveWithObjectIntent);
                 break;
+
+            case R.id.btn_dial_number:
+                String phoneNumber = "085959337037";
+                Intent dailPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(dailPhoneIntent);
+                break;
+
+            case R.id.btn_move_for_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+                startActivityForResult(moveForResultIntent, REQUEST_CODE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MoveForResultActivity.RESULT_CODE) {
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText("Hasil : " + selectedValue);
+            }
         }
     }
 }
